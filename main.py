4728e55300976ef0425db8771f4d1e939b38b182
klebioreudo <56925275+klebioreudo@users.noreply.github.com>
2020-03-25 18:50:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import json
 import os
 from selenium import webdriver
 from time import sleep
+import random
+
 
 def main():
-    codigoS = input()
-    senhaS = input()
+    codigoS = input('Código da sala: ')
+    senhaS = input('Senha da sala: ')
+    nomeJ = input('Nome no jogo: ')
+
+    vocabulario = json.loads(open('dictionary.json', 'r').read())
+
     path = os.path.dirname(os.path.realpath(__file__))
     driver = webdriver.Chrome(path + '/chromedriver')
     driver.get('https://stopots.com.br')
@@ -15,14 +22,14 @@ def main():
     driver.implicitly_wait(15)
     nome = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/input')
     nome.clear()
-    sleep(2)
-    nome.send_keys('descubrah')
+    sleep(5)
+    nome.send_keys(nomeJ)
     driver.implicitly_wait(15)
     salaC = driver.find_element_by_xpath('//*[@id="screenHome"]/div[2]/div[2]/button[3]')
     salaC.click()
     buscaS = driver.find_element_by_xpath('//*[@id="search"]')
     buscaS.send_keys(codigoS)
-    sleep(2)
+    sleep(5)
     entraS = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div/div[1]/ul/li[1]')
     entraS.click()
     entrar = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div[3]/button[2]')
@@ -33,10 +40,29 @@ def main():
         senha.send_keys(senhaS)
         entrarSenha = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/form/div[2]/button')
         entrarSenha.click()
-    sleep(10)
+    sleep(2)
     driver.implicitly_wait(15)
+
     while True:
-        letra = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/ul/li[1]/span')
+        op = input("Digite 'sair' para sair: ")
+        if op == 'sair':
+            break
+        letra = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/ul/li[1]/span').text
+        letra = str(letra).lower()
+
+        for i in range(1, 13):
+            try:
+                categoria = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div[1]/label[' + str(i) + ']/span').text
+                categoria = str(categoria).lower()
+                if categoria == 'nome':
+                    categoria = 'nome masculino'
+                retornoP = random.choice(vocabulario[letra][categoria])
+                resposta = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div[1]/label[' + str(i) + ']/input')
+                resposta.click()
+                resposta.send_keys(retornoP)
+                sleep(0.2)
+            except:
+                pass
 
 
 if __name__ == '__main__':
